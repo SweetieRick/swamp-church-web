be8225a7c4117bdd72e1d288ba19e33edd65a6bf
SweetieRick <65025304+SweetieRick@users.noreply.github.com>
2021-09-23 18:42:56 +0200

    Started making homepage

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+</script>
+
+<main>
+  <h1 transition:fade>
+    Swamp Church
+  </h1>
+</main>
+
+<style>
+  :root {
+    background-color: rgb(44, 47, 51);
+  }
+
+  h1 {
+    text-align: center;
+    font-family:' Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 60px;
+    margin: 250px 250px 250px 250px;
+  }
+</style>
